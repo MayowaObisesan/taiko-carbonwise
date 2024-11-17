4aab479279b50af8abd32049985200529cd6340d
MayowaObisesan <mayowaobi74@gmail.com>
2024-11-17 10:37:02 -0800

    update build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    globalThis: "globalThis",
-  },
+  // define: {
+  //   global: "globalThis",
+  // },
   resolve: {
     alias: {
       process: "process/browser",
